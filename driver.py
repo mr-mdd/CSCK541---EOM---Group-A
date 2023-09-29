a48e5a3368456f2d20cf17e12224a4e61b88b259
Daniel Davis <143042048+Snake-Trial@users.noreply.github.com>
2023-09-29 11:16:26 +0100

    Wizard for choosing the Server Options

---
+++ driver.py
@@ -0,0 +1,12 @@
+"""Module for running the Branston Classes"""
+# Author: Daniel Davis
+# Group: CSCK451 Group A
+# Date: 29/09/2023
+
+from swizard import ServerWizard
+
+
+# Server Wizard Driver Code *******************************************
+my_wizard = ServerWizard()
+my_wizard.ask_all()
+my_wizard.display()
