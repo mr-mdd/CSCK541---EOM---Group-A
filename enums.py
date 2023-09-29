4cbdedd5eadf4cc593c45d76c0e441e8e71cbe42
Daniel Davis <143042048+Snake-Trial@users.noreply.github.com>
2023-09-29 15:33:59 +0100

    Add files via upload

---
+++ enums.py
@@ -0,0 +1,44 @@
+"""Module containing enumerated types"""
+# Author: Daniel Davis
+# Group: CSCK451 Group A
+# Date: 27/09/2023
+
+from enum import Enum
+
+
+class Format(Enum):
+    """Pickling Format for data dictionary"""
+    # Used by Client and Server
+    BINARY = 1
+    JSON = 2
+    XML = 3
+
+
+class Source(Enum):
+    """Data source format"""
+    # Used by Client and Server
+    Dictionary = 4
+    TextFile = 8
+
+
+class SecurityLevel(Enum):
+    # Used by Client and Server
+    UnEncrypted = 16
+    Encrypted = 32
+
+
+class Conversation(Enum):
+    # Used by Client and Server
+    CanISendData = 256
+    SendDataFormat = 257
+    PublicKeyReceived = 258
+    SendData = 259
+    DataReceived = 260
+    ErrorReadingData = 261
+
+
+class ServerDestination(Enum):
+    """Server output option"""
+    # Used only by Server
+    Print = 512
+    File = 1028
